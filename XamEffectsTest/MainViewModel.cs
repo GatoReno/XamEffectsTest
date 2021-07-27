cff8a3d0236d6f5811dd83c4bfa9a3c469e6ae75
Ed <pushpoped@gmail.com>
2021-07-27 09:26:59 -0500

    first commit

---
+++ XamEffectsTest/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamEffectsTest
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _longTap, _tap;
+
+        public int LongTap
+        {
+            get
+            {
+                return _longTap;
+            }
+            set
+            {
+                _longTap = value;
+                OnPropertyChanged("LongTap");
+            }
+        }
+        public int Tap
+        {
+            get
+            {
+                return _tap;
+            }
+            set
+            {
+                _tap = value;
+                OnPropertyChanged("Tap");
+            }
+        }
+
+
+        public ICommand LongTapCommand { get; set; }
+        public ICommand TapCommand { get; set; }
+
+        public MainViewModel()
+        {
+            LongTapCommand = new Command(LongTapCommandExecute);
+            TapCommand = new Command(TapCommandExecute);
+            LongTap = 0;
+            Tap = 0;
+        }
+
+        private void TapCommandExecute()
+        {
+           Tap = Tap + 1;
+        }
+
+        private void LongTapCommandExecute()
+        {
+            LongTap = LongTap + 1;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
